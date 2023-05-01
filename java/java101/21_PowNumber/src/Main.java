ed6d7b53e73c003406672fe42f56774cc4ea56e7
omerr02200 <omerr_02200@hotmail.com>
2023-05-01 17:14:20 +0300

    Sayının Üssü

---
+++ java/java101/21_PowNumber/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int n, k;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Üssü alınacak sayı: ");
+		n = input.nextInt();
+		
+		System.out.print("Üs olacak sayı: ");
+		k = input.nextInt();
+		
+		int total = 1;
+		
+		/*
+		while(k >= 1) {
+			total *= n;
+			k--;
+		}
+		*/
+		
+		for(int i=1; i <= k; i++) {
+			total *= n;
+		}
+		
+		System.out.println("Sonuç: " + total);
+}
+}
